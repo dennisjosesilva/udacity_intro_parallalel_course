a100c5c8608faa57cd6559856cc97865223b48f2
dennisjosesilva <dennisjosesilva@gmail.com>
2014-08-20 00:43:02 -0300

    Grey scale conversion - first version, not parellel solution

--- ProblemSet_01-GreyScaleImage/ProblemSet_01-GreyScaleImage/main.cpp
+++ ProblemSet_01-GreyScaleImage/ProblemSet_01-GreyScaleImage/main.cpp
@@ -1,8 +1,14 @@
 #include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <cuda.h>
+#include <cuda_runtime.h>
 #include <iostream>
 
 using namespace std;
 using namespace cv;
+
+Mat convertToGreyScaleImage(Mat& colouredImage);
 
 int main(int argc, char** argv)
 {
@@ -27,12 +33,28 @@ int main(int argc, char** argv)
 	}
 	else
 	{
-		namedWindow("Hello World Image", WINDOW_AUTOSIZE);
-		imshow("Hello World Image", image);
-
-		waitKey(0);
+		Mat colouredImage;
+		cvtColor(image, colouredImage, CV_BGR2RGBA);
+		auto greyImage = convertToGreyScaleImage(colouredImage);
 		
+		namedWindow("Hello World Image", WINDOW_AUTOSIZE);
+		imshow("Hello World Image", greyImage);
+		
+		waitKey(0);
 	}
 	
 	return 0;
+}
+
+Mat convertToGreyScaleImage(Mat& colouredImage)
+{
+	auto greyImage = Mat(colouredImage.rows, colouredImage.cols, CV_8UC1);
+	auto ptrGreyImage = greyImage.ptr<unsigned char>(0);
+	auto ptrColouredImage = (uchar4 *)colouredImage.ptr<unsigned char>(0);
+	auto numPixels = colouredImage.total();
+
+	for(auto i = 0; i < numPixels; i++)
+		ptrGreyImage[i] = (.299f * ptrColouredImage[i].x) + (.587f * ptrColouredImage[i].y) + (.114 * ptrColouredImage[i].z);
+
+	return greyImage;
 }
